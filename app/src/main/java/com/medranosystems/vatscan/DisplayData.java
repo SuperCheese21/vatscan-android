5f8560a479abce23e2f427dd76316f252ee65301
SuperCheese21 <supercheese21@comcast.net>
2017-07-09 04:01:31 -0700

    Reorganized methods / more code cleanup

--- app/src/main/java/com/medranosystems/vatscan/DisplayData.java
+++ app/src/main/java/com/medranosystems/vatscan/DisplayData.java
@@ -1,6 +1,10 @@
 package com.medranosystems.vatscan;
 
+import android.graphics.Color;
+
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -14,37 +18,53 @@ import java.util.Objects;
 
 public class DisplayData {
 
-    public static void displayPilots(String rawData, GoogleMap mMap) {
-        System.out.println("displayPilots()");
-        List<Pilot> pilots = parseData(rawData);
-        addPilots(pilots, mMap);
-    }
-
-    public static List<Pilot> parseData(String s) {
+    public static void parseClients(String s, GoogleMap map) {
         String[] clientsRaw = (s.split("!CLIENTS:\n")[1]).split("\n;\n;\n!SERVERS:")[0].split("\n");
-        List<Pilot> pilots = new ArrayList<Pilot>();
+        List<Pilot> pilots = new ArrayList<>();
+        List<Controller> controllers = new ArrayList<>();
 
         for (int i = 0; i < clientsRaw.length; i++) {
             String[] data = clientsRaw[i].split(":");
 
             if (Objects.equals(data[3], "PILOT")) {
                 pilots.add(new Pilot(data));
+            } else if (Objects.equals(data[3], "ATC")) {
+                controllers.add(new Controller(data));
             }
         }
 
-        return pilots;
+        displayClients(pilots, controllers, map);
     }
 
-    public static void addPilots(List<Pilot> pilots, GoogleMap map) {
+    private static void displayClients(List<Pilot> pilots, List<Controller> controllers, GoogleMap map) {
         map.clear();
-
         for (Pilot p : pilots) {
             LatLng location = new LatLng(p.latitude, p.longitude);
             map.addMarker(new MarkerOptions()
                     .position(location)
                     .title(p.callsign)
-                    .snippet(p.realname + "\n")
+                    .snippet(p.realname)
+            );
+        }
+
+        for (Controller c : controllers) {
+            LatLng location = new LatLng(c.latitude, c.longitude);
+            int radius = c.visualrange;
+
+            map.addMarker(new MarkerOptions()
+                .position(location)
+                .title(c.callsign)
+                .snippet(c.realname)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+            );
+            map.addCircle(new CircleOptions()
+                .center(location)
+                .radius(radius * 185.2)
+                .strokeColor(Color.BLUE)
+                .strokeWidth(3)
+                .fillColor(Color.argb(40, 0, 0, 255))
             );
         }
     }
+
 }
--- app/src/main/java/com/medranosystems/vatscan/MapsActivity.java
+++ app/src/main/java/com/medranosystems/vatscan/MapsActivity.java
@@ -31,21 +31,11 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -70,12 +60,12 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         t.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                updateData();
+                pullUpdate();
             }
         }, 0, 60000);
     }
 
-    private void updateData() {
+    private void pullUpdate() {
         final int rand = ThreadLocalRandom.current().nextInt(0, URLS.length);
         final ProgressBar mProgressBar = (ProgressBar) findViewById(R.id.loadProgress);
         final AsyncResponse response = this;
@@ -92,9 +82,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void processFinish(String output){
-        System.out.println("Size: " + output.length());
-
-        DisplayData.displayPilots(output, mMap);
+        DisplayData.parseClients(output, mMap);
     }
 
 }
